aabed3bc54a2c27da4bdc897f2c7c6c8f568596e
Sergey Badrizlov <106447328+KyouGOD@users.noreply.github.com>
2023-04-04 15:21:24 +0600

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,25 @@
+import logging
+from aiogram import Bot, Dispatcher, executor, types
+from config import TOKEN
+import sched, time
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def process_start_command(message: types.Message):
+    await message.reply("Привет!\nНапиши мне что-нибудь!")
+
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+
+@dp.message_handler(commands=['help'])
+async def process_help_command(message: types.Message):
+    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+
+
+@dp.message_handler()
+async def echo_message(msg: types.Message):
+    await bot.send_message(msg.from_user.id, msg.text)
